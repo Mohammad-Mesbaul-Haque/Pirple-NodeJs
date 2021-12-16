9a034f101bea95d9798d73410cf79e1dfe8bb7b4
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-12-16 16:44:40 +0600

    index file initiated

---
+++ index.js
@@ -0,0 +1,19 @@
+ /*
+ * Title: First Module.
+* Description: First Module (Basic Scaffolding) 
+* Author: Mohammad Mesbaul Haque
+* Github link: https://github.com/mohammad-mesbaul-haque/pirple-nodejs
+* Date: 16/12/2021
+*/
+ 
+// Dependencies
+ 
+// App object or Module scaffolding. 
+ 
+// main functions or objects.
+  
+ 
+ 
+ 
+ // export the module. 
+// module.exports = ; 
