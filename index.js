c607087b7e409248b531b0425b432e75ad40ea3e
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-12-26 16:13:53 +0600

    Token functionality initiated

--- index.js
+++ index.js
@@ -102,7 +102,8 @@ let unifiedServer = (req, res)=>{
 // Define a request router
 var router = {
     'ping': handlers.ping,
-    'users': handlers.users
+    'users': handlers.users,
+    'tokens' : handlers.tokens
 }
 
 
