ea5eebf999f3f572cace83c2d966e75d70c06825
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-01-02 17:45:23 +0600

    Logging tricks file initiated

---
+++ logging-tricks.js
@@ -0,0 +1,15 @@
+/*
+ * Title: Basic Logging tricks
+ * Description: Basic Logging tricks
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mmesba
+ * Date: 02/01/2022
+ */
+ 
+// Dependencies.
+ 
+ 
+ 
+ 
+// export the module.
+ 
