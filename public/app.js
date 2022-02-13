62c06636c58ee06c377e4c4b884f5436f6137784
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-02-13 23:41:02 +0600

    client side app js file initiated

---
+++ public/app.js
@@ -0,0 +1,20 @@
+/*
+ * Title: Frontend Logic For the Application
+ * Description: Client side js file for application
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mmesba
+ * Date: 13/02/2022
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+const app = {} 
+// main functions or objects.
+ console.log('Hello World');
+ 
+ 
+ 
+// export the module.
+ module.exports = app
