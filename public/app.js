b7b0a6e621e2aa486caba427ee7173f881fea345
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-02-16 23:57:03 +0600

    app's client side js file writting, and will be continued

--- public/app.js
+++ public/app.js
@@ -11,10 +11,31 @@
  
 // App object or Module scaffolding.
 const app = {} 
+
+// Config
+app.config = {
+    'sessionTokens' : false
+}
 // main functions or objects.
- console.log('Hello World');
- 
+//  AJAX Client   (for the restful api)
+app.client = {};
+
+// Interface for making API calls
+app.client.request = (headers, path, method, queryStringObject, payload, callback)=>{
+    // Set defaults
+    headers = typeof(headers) === 'object' && headers !==null ? headers : {};
+    path = typeof(path) === 'string' ? path : '/';
+    method = typeof(method) === 'string' && ['POST', 'GET', 'PUT', 'DELETE'].indexOf(method) > -1 ? method.toUpperCase() : 'GET';
+    queryStringObject = typeof(queryStringObject) === 'object' && queryStringObject !== null ? queryStringObject : {};
+    payload = typeof(payload) === 'object' && payload !== null ? payload : {};
+    callback = typeof(callback) === 'function' ? callback : false;
+
+    // For each query string parameter sent, add it to the path
+    let request = path+'?';
+    let counter = 0;
+    
+}
  
  
 // export the module.
- module.exports = app
+//  module.exports = app
